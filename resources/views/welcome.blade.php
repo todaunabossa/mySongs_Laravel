fe48bee2c3b46c380c632753d7db9a4714dd4985
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-08-17 17:22:08 +0200

    css settings welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <title>Portada con Foto</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 </head>
 <body>
     <div class="welcome-container">
-        <img src="{{ asset('images/welcome.jpg') }}" alt="welcome">
+        <img src="{{ asset('images/welcome.png') }}" alt="Logo Spotify">
     </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', 'welcomeController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
