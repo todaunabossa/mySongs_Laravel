b4797c8ae10eb35e0dd83570a41f90e1cc889a8b
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-08-17 22:59:59 +0200

    create DB and Model

--- database/migrations/2014_10_12_000000_create_songs_table.php
+++ database/migrations/2014_10_12_000000_create_songs_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->binary('photo');
+            $table->string('title');
+            $table->string('album');
+            $table->string('date');
+            $table->time('duration')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('songs');
     }
 };
