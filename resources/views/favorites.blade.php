58554987328fa04ef3b8c3e3781520bc56f6a57a
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-08-18 09:34:39 +0200

    table structural fixes

--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -7,18 +7,37 @@
        @import url('https://fonts.googleapis.com/css2?family=Changa+One&family=Cinzel:wght@400;700&family=Jolly+Lodger&family=Pangolin&family=Permanent+Marker&family=Roboto:wght@100&display=swap');
     </style>
 </head>
+
 <body>
+
     <h1>Favorite Songs List</h1>
     <div class="songs-list">
-        <ul>
-            @foreach ($songs as $song)
-                <li>{{ $song->id }}</li>
-                <li>{{ $song->title }}</li>
-                <li>{{ $song->album }}</li>
-                <li>{{ $song->year }}</li>
-            @endforeach
-        </ul>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Album</th>
+                    <th>Year</th>
+                    <th>Created at</th>
+                    <th>Duration</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($songs as $song)
+                    <tr>
+                        <td>{{ $song->id }}</td>
+                        <td>{{ $song->title }}</td>
+                        <td>{{ $song->album }}</td>
+                        <td>{{ $song->year }}</td>
+                        <td>{{ $song->created_at }}</td>
+                        <td>{{ $song->duration }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </body>
+
 </html>
 
